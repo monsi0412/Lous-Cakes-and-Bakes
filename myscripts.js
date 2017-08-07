a2beb3ab4ed6786ebeb1d337f70a65668b957029
Monserrat <monserratdiaz12@gmail.com>
2017-08-07 19:15:39 +0000

    more responsive touches

--- myscripts.js
+++ myscripts.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 $(".checkout").click(function(){
     var quantity = $(".qty").val();
     console.log(quantity);
+    console.log(2+2);
+    $("#orderSummary").append("<p>"+quantity+"</p>");
 });
     
     
