8bfc4218f95f5812cbc6aacfa95b6cb7bca72577
Monserrat <monserratdiaz12@gmail.com>
2017-08-08 19:25:46 +0000

    touch ups

--- myscripts.js
+++ myscripts.js
@@ -2,13 +2,19 @@
 
 $(document).ready(function(){
     
-$(".checkout").click(function(){
-    var quantity = $(".qty").val();
-    console.log(quantity);
-    console.log(2+2);
-    $("#orderSummary").append("<p>"+quantity+"</p>");
-});
+// $(".checkout").click(function(){
+//     var quantity = $(".qty").val();
+//     console.log(quantity);
+//     console.log(2+2);
+//     $("#orderSummary").append("<p>"+quantity+"</p>");
+//     $("#orderSummary").append("<p> This is working</p>");
+//     $("#orderSummary").css("background-color", "red");
+//     alert("moakonsfnds");
+// });
+
+function checkout () {
     
+}
     
     
     
